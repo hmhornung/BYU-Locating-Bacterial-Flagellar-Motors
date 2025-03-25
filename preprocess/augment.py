eb5a6ac8fa21a54983c42b012f7592b71fdcb364
hmhornung <85147335+hmhornung@users.noreply.github.com>
2025-03-24 23:48:31 -0500

    preprocessing to disk functions

--- preprocess/augment.py
+++ preprocess/augment.py
@@ -1,24 +1,115 @@
 import numpy as np
-
+import torch
+import random
 from scipy.ndimage import gaussian_filter
 from monai.transforms import (
     Compose,
     NormalizeIntensityd,
     RandFlipd,
-    RandAffined,
     RandSpatialCropd,
-    SpatialPadd,
     ResizeWithPadOrCropd,
     RandRotated,
-    SqueezeDimd
+    ToDeviced,
+    Zoomd
 )
+
+aug_params = {
+    "patch_size": (100,100,100),
+    "final_size":   (100,100,100),
+    "flip_prob":  0.5,
+    "rot_prob":   0.5,
+    "scale_prob": 1.0,
+    "rot_range":  np.pi,
+    "scale_range": 0.2
+}
 
 def get_heatmap(shape, pts, radius):
     volume = np.zeros(shape, dtype=np.float32)
     for pt in pts: 
         pt = tuple(pt)
         volume[pt] = 1.0
-    volume = gaussian_filter(volume, radius, axes=(0,1,2))
+    volume = gaussian_filter(
+        volume, 
+        radius, 
+        axes=(0,1,2)
+        )
     volume = volume / np.max(volume)
     return volume.astype(np.float16)
 
+def rand_aug(
+    src:torch.tensor,
+    tgt:torch.tensor,
+    aug_params=None,
+    gpu:bool=True
+):
+    """
+    Augment 3D volume and mask with cropping, normalization, padding, flipping, and rotation.
+    The implementation allows for efficient data loading.
+    Input should be memory mapped torch tensors on CPU
+
+    Parameters:
+        src (torch.tensor): source 3D volume (shape: D x H x W).
+        tgt (torch.tensor): target 3D volume (shape: D x H x W).
+        aug_params (Dict): 
+    Returns:
+        transformed source / target dict
+    """
+    device='cpu'
+    if gpu: device='cuda'
+
+    if len(src.shape) == 3: src = src.unsqueeze(0)
+    if len(tgt.shape) == 3: tgt = tgt.unsqueeze(0)
+
+    # Convert to tensors and move to the specified device
+    sample = {"src": src, "tgt": tgt}
+
+    keys = ["src", "tgt"]
+    mode = ['bilinear', 'bilinear']
+
+    scale_range = [random.uniform(1.0-aug_params['scale_range'], 1.0+aug_params['scale_range']) for i in range(3)]
+
+    augment = Compose([
+        RandSpatialCropd(
+            keys=keys, 
+            roi_size=aug_params["patch_size"], 
+            random_center=True, 
+            random_size=False
+        ),
+        ToDeviced(
+            keys=keys,
+            device=device
+        ),
+        NormalizeIntensityd( # need in inference
+            subtrahend=torch.mean(sample['src']),
+            divisor=torch.std(sample['src']),
+            keys="src"
+        ), 
+        RandFlipd(
+            keys=keys,
+            spatial_axis=[0, 1, 2], 
+            prob=aug_params["flip_prob"]
+        ),
+        Zoomd(
+            keys=keys,
+            zoom=scale_range,
+            mode='trilinear',
+            keep_size=False,
+            padding_mode='zeros'
+        ),
+        RandRotated(
+            keys=keys, 
+            range_x=aug_params["rot_range"], 
+            prob=aug_params["rot_prob"],  
+            keep_size=False,
+            padding_mode='zeros',
+            mode=mode
+        ),
+        ResizeWithPadOrCropd(
+            keys=keys, 
+            spatial_size=aug_params["final_size"], 
+            method="symmetric",
+            mode="constant"
+        )
+    ])
+    
+    return augment(sample)
--- preprocess/load.py
+++ preprocess/load.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pandas as pd
+import torch
 import PIL.Image as Image
 import os
+import augment
 
 def load_tomo(path):
     filenames = os.listdir(path)
@@ -8,3 +11,45 @@ def load_tomo(path):
     for file in filenames: 
         slices.append(Image.open(os.path.join(path,file)))
     return np.stack(slices, dtype=np.float16)
+
+def load_pts(name, path='../data/train_labels.csv'):
+    df = pd.read_csv(path)
+    rows = df.loc[df['tomo_id'] == 'name']
+    print(rows)
+    pts = []
+    for i in range(len(rows)):
+        pts.append([rows.iloc[i][f'Motor axis {j}'] for j in range(3)])
+    return pts
+
+def save_sample(path, name:str, sample:dict):
+    '''
+    Save a sample dict of torch tensor src/tgt to disk
+    Parameters:
+        path
+        name (string): name of file (exclude .pt)
+        sample: (dict): dict{'src','tgt'} mapping to torch tensor volumes
+    '''
+    src_dest = os.path.join(path, 'src/', name + '.pt')
+    tgt_dest = os.path.join(path, 'tgt/', name + '.pt')
+    torch.save(sample['src'], src_dest)
+    torch.save(sample['tgt'], tgt_dest)
+
+def gen_heatmap_dataset(dest:os.path, tomo_src:os.path='../data/train', pt_src='../data/train_labels.csv', radius=30):
+    tomos = os.listdir(tomo_src)
+    for tomo in tomos:
+        src = load_tomo(os.path.join(tomo_src, tomo))
+        pts = load_pts(tomo, path=pt_src)
+        tgt = augment.get_heatmap(
+            shape=src.shape, 
+            pts=pts, 
+            radius=radius
+            )
+
+        src = torch.Tensor(src).to(torch.float16)
+        tgt = torch.Tensor(tgt).to(torch.float16)
+        sample = {'src':src, 'tgt':tgt}
+        save_sample(
+            path=dest, 
+            name=tomo, 
+            sample=sample
+            )
